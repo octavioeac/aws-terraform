1981ab7896b1ececc89079ce63f49fbcfa8689d4
atractorcrew <atractorcrew@gmail.com>
2025-10-02 20:01:12 -0600

    chore: Add the provider from aws

--- infra/modules/ECR/outputs.tf
+++
@@ -1,5 +0,0 @@
-output "repository_urls" {
-  value = { 
-    for k, r in aws_ecr_repository.this : k => r.repository_url 
-    }
-}
--- infra/providers.tf
+++ infra/providers.tf
@@ -1,24 +1,10 @@
 terraform {
-  required_version = ">= 1.5.0"
-
   required_providers {
-    aws = { source = "hashicorp/aws", version = "~> 5.0" }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.92"
+    }
   }
 
-  # Opcional: backend remoto (recomendado). Si aún no lo tienes, quita este bloque
-  backend "s3" {
-    bucket         = "octavio-tf-state-160027200171-us-east-2"  # <-- tu bucket de estado
-    key            = "envs/dev/terraform.tfstate"
-    region         = "us-east-2"
-    dynamodb_table = "octavio-tf-locks"                         # <-- tu tabla de locks
-    encrypt        = true
-  }
+  required_version = ">= 1.2"
 }
-
-provider "aws" {
-  region = var.region
-}
-
-# Útiles para outputs
-data "aws_caller_identity" "current" {}
-data "aws_region" "current" {}
