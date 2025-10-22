de51fafa92019aa805f3c2496dad524c7d245a69
atractorcrew <atractorcrew@gmail.com>
2025-10-21 23:23:10 -0600

    feature: Add the force_delete to protected the expcetion in ECR terraform

--- infra/modules/ECR/main.tf
+++ infra/modules/ECR/main.tf
@@ -1,10 +1,10 @@
 
 resource "aws_ecr_repository" "aws_ecr_repository_java" {
   name = var.name-ecr-java
-
+  force_delete = var.ecr_force_delete
 }
-
 
 resource "aws_ecr_repository" "aws_ecr_repository_node" {
   name = var.name-ecr-node
+  force_delete = var.ecr_force_delete
 }
